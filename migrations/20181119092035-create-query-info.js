645fc0ed07f8b0973e2bf0962c9b69faf26c5b14
Luis Silva <varsnotwars@protonmail.com>
2018-11-19 17:09:43 +0700

    Created query_info using enums data types

---
+++ migrations/20181119092035-create-query-info.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('query_info', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(10).UNSIGNED
+      },
+      report_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: "reports",
+          key: "id"
+        }
+      },
+      address_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER(10).UNSIGNED,
+        references: {
+          model: "addresses",
+          key: "id"
+        }
+      },
+      number_habitable_rooms: Sequelize.INTEGER,
+      built_from: Sequelize.ENUM('Enclosed End-Terrace', 'Enclosed Mid-Terrace', 'End-Terrace', 'Mid-Terrace', 'Semi-Detached', 'Detached'),
+      total_floor_area: Sequelize.INTEGER,
+      wall_type: Sequelize.ENUM('brick', 'cavity wall', 'timber', 'stone', 'cob', 'unknown'),
+      property_type : Sequelize.ENUM('Flat', 'Maisonette', 'Bungalow', 'House')
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('query_info');
+  }
+};
